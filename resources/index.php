144a796415d1498c4abe5793b6b4a9468ed4a86f
Sebastião De Carvalho <sebastiaol.carvalho22@gmail.com>
2024-05-26 19:10:16 +0100

    Version 1.1

---
+++ resources/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <!-- Boxicons CSS -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="./css/style-inicio.css">
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar">
+        <div class="container">
+            <div class="side_left">
+                <span class="logo">
+                  <i class='bx bxs-buildings'></i>
+                </span>
+                <span class="name_system">
+                    <h2>CondoMax</h2>
+                </span>
+            </div>
+            <div class="side_right">
+                <ul class="links">
+                    <li class="list">
+                        <a href="">Inicio</a>
+                    </li>
+                    <li class="list">
+                        <a href="">Registar</a>
+                    </li>
+                    <li class="list">
+                        <a href="">Notícias</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Main -->
+    <main class="container2">
+        <div class="slideshow-container">
+            <div class="mySlides fade">
+                <img src="./images/1.webp">
+            </div>
+            <div class="mySlides fade">
+                <img src="./images/2.jpg">
+            </div>
+            <div class="mySlides fade">
+                <img src="./images/3.jpg">
+            </div>
+            <div class="text">
+                <span>Construindo uma </br> comunidade unida através <br>de uma gestão eficaz.</span>
+            </div>
+            <div class="button-login">
+                <button type="button">LOGIN</button>
+            </div>
+        </div>
+    </main>
+    <script src="./js/iterator.js"></script>
+</body>
+</html>
