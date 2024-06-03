edaad53b2deee9327861a0098828d4583fb6b425
Sebastião De Carvalho <sebastiaol.carvalho22@gmail.com>
2024-06-03 15:54:17 +0100

    Version 1.3

--- resources/index.php
+++ resources/index.php
@@ -58,18 +58,28 @@
     </main>
     <section class="container3">
         <div class="option">
-            <div class="about">Sobre Nós</div>
-            <div class="management">Gestão de Condomínios</div>
-            <div class="contact">Contactos</div>
+            <div class="about"><span class="info">Sobre Nós</span></div>
+            <div class="management"><span class="info">Gestão de Condomínios</span></div>
+            <div class="contact"><span class="info">Contactos</span></div>
         </div>
         <div class="content">
             <h1 class="options">GESTÃO DE CONDOMÍNIOS</h1>
-            <p>
+            <div class="info">
             Administrar um condomínio hoje em dia é quase o equivalente a gerir uma empresa. São empreendimentos que necessitam atenção cautelosa e dedicação por parte de profissionais competentes, que felizmente temos disponíveis para si.
             Atuamos na administração de condomínios com atendimento personalizado, voltados para condomínios horizontais ou verticais.
-            </p>
+            </div>
+            <div class="imgs">
+                <img src="./images/4.jpg" alt="condo" class="condo-img">
+                <img src="./images/10.jpg" alt="condo" class="condo-img">
+            </div>            
         </div>
     </section>
+    <footer class="footer">
+        <div class="container">
+            <span class="copyright">Copyright ©2024. Todos os direitos reservados. CondoMax</span>
+            <span class="politic">Politica de privacidade</span>
+        </div>
+    </footer>
     <script src="./js/iterator.js"></script>
 </body>
 </html>
