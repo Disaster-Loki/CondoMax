4d9a1b348baf2b7c91ba964ef1e98c364e0e36a2
Disaster-Loki <sebastiaol.carvalho22@gmail.com>
2024-07-01 10:27:50 -0500

    Version 3.2

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 require_once './backend/model/funcionario.php';
 require_once './backend/controller/funcionario.php';
-
+require_once './backend/model/condominio.php';
+require_once './backend/controller/condominio.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-$func = new Funcionario($conn);
 
 ?>
